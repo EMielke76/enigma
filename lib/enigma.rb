409db2735deabedcb187130b1552895f127b71a1
Eric Mielke <BeardBikesandBeers@gmail.com>
2021-11-14 14:53:03 -0700

    Feat: #cypher is passing all tests

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,20 +1,22 @@
 require 'date'
 require 'dateable'
 require 'keyable'
+require 'encryptable'
 
 class Enigma
   include Dateable
   include Keyable
+  include Encryptable
   attr_reader :message,
               :key,
               :date,
-              :char_array
+              :valid_chars
 
   def initialize
-    @message    = ""
-    @key        = ""
-    @date       = ""
-    @char_array = ('a'..'z').to_a.push(' ')
+    @message    = ''
+    @key        = ''
+    @date       = ''
+    @valid_chars = ('a'..'z').to_a.push(' ')
   end
 
   def key_maker
@@ -35,7 +37,27 @@ class Enigma
     offset.each_char.with_index do |char, index|
       shift_values.push(char.to_i + key.slice(index..index+1).to_i)
     end
-    #require "pry"; binding.pry
     shift_values
+  end
+
+  def cypher(message, shift)
+    encrypted_message = ''
+    down_boy = message.downcase
+    down_boy.each_char do |char|
+      if @valid_chars.include?(char) == false
+        encrypted_message.concat(char)
+      else
+        new_char = @valid_chars.index(char) + shift[0]
+        encrypted_message.concat(@valid_chars.rotate(new_char)[0])
+        shift.rotate!
+      end 
+    end
+    encrypted_message
+  end
+
+  def encrypt(message, key = key_maker, date = time_lord)
+    @message = message
+    @key = key
+    @date = date
   end
 end
