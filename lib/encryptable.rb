8d68374893e87bf29ab1dda3e3c2ff2f356a2339
Eric Mielke <BeardBikesandBeers@gmail.com>
2021-11-14 14:53:47 -0700

    Feat: Prep encryptable module for use

--- lib/encryptable.rb
+++ lib/encryptable.rb
@@ -5,11 +5,4 @@ require 'date'
 module Encryptable
   include Keyable
   include Dateable
-
-  def the_shift
-    ("a".."z").to_a..push(" ")
-  end
-
-  def encrypt(message, key = random_key, date = default_date)
-  end
 end
