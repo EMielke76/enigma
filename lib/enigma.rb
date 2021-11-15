41b277204aab5344fc1fc5fd12950edf9eba8495
Eric Mielke <BeardBikesandBeers@gmail.com>
2021-11-15 12:44:12 -0700

    Feat: #decrypt is passing all tests

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -2,31 +2,44 @@ require 'date'
 require_relative 'dateable'
 require_relative 'keyable'
 require_relative 'encryptable'
-require_relative 'fileable'
 
 class Enigma
   include Dateable
   include Keyable
   include Encryptable
-  include Fileable
   attr_reader :message,
               :key,
               :date
 
+  attr_accessor :encrypted_data,
+                :decrypted_data
+
   def initialize
-    @message    = ''
-    @key        = ''
-    @date       = ''
+    @message        = ''
+    @key            = ''
+    @date           = ''
+    @encrypted_data = nil
+    @decrypted_data = nil
   end
 
   def encrypt(message, key = random_key, date = default_date)
     @message = message
-    @key = key
-    @date = date
-    offset = offset_value(@date)
-    shift = the_shift(offset, @key)
+    @key     = key
+    @date    = date
+    shift = shift(@key, @date)
     puts "Created 'encrypted.txt' with the key #{@key} and date #{@date}"
-    encrytped_hash = {encryption: cypher(@message, shift), key: @key, date: @date}
+    @encrypted_data = {encryption: cypher(@message, shift), key: @key, date: @date}
   end
+  #require "pry"; binding.pry
 
+
+
+  def decrypt(message, key = @encrypted_data[:key], date = @encrypted_data[:date])
+    @message = message
+    @key     = key
+    @date    = date
+    shift = shift(@key, @date)
+    puts "Created 'decrypted.txt' with the key #{@key} and date #{@date}"
+    @decrypted_data = {decryption: decypher(@message, shift), key: @key, date: @date}
+  end
 end
