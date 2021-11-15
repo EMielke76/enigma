78f9d63e222dd20d6b69c9ea6c25e3baef623db0
Eric Mielke <BeardBikesandBeers@gmail.com>
2021-11-14 22:06:43 -0700

    Feat: Encrypt runner is now printing results to encrypted.txt

--- encrypt.rb
+++ encrypt.rb
@@ -4,4 +4,10 @@ open_file = File.open(ARGV[0], "r")
 message = open_file.read
 open_file.close
 enigma = Enigma.new
-enigma.encrypt((message.chomp))
+encrypted = enigma.encrypt((message.chomp))
+
+writer = File.open(ARGV[1], "w")
+#require "pry"; binding.pry
+writer.write(encrypted[:encryption])
+
+writer.close
